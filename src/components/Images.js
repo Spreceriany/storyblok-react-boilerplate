ac1e1b3737a013689682c83a483e29bae2425325
Patrik Horný <patulda111@gmail.com>
2023-10-15 18:00:31 +0200

    Update Images.js

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,27 +1,31 @@
 import { StoryblokComponent, storyblokEditable } from "@storyblok/react";
-
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/opacity.css";
 const Images = ({ blok }) => {
   console.log(blok);
 
   const images = blok.images;
   const photosContainer = document.getElementById("photos");
+  function preloadImage(image) {
 
-  images.forEach((image, index) => {
-    const img = new Image();
-    img.src = image.filename + "/m/";
-    img.alt = image.filename + "/m/";
+    images.forEach((image, index) => {
+      const img = new Image();
+      img.src = image.filename + "/m/";
+      img.alt = image.filename + "/m/";
+  
+      img.onload = () => {
+        // Append the image to the container when it's loaded
+        photosContainer.appendChild(img);
+  
+        // Preload the next image
+        const nextIndex = index + 1;
+        if (nextIndex < images.length) {
+          preloadImage(images[nextIndex]);
+        }
+      };
+    });
+  }
 
-    img.onload = () => {
-      // Append the image to the container when it's loaded
-      photosContainer.appendChild(img);
-
-      // Preload the next image
-      const nextIndex = index + 1;
-      if (nextIndex < images.length) {
-        preloadImage(images[nextIndex]);
-      }
-    };
-  });
 
   // Preload the first image
   preloadImage(images[0]);
