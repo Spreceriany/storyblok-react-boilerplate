b1faa49b11f342826a40c29fd40a0d87c6e70433
Patrik Horný <patulda111@gmail.com>
2023-10-15 18:02:11 +0200

    Update Images.js

--- src/components/Images.js
+++ src/components/Images.js
@@ -4,32 +4,6 @@ import "react-lazy-load-image-component/src/effects/opacity.css";
 const Images = ({ blok }) => {
   console.log(blok);
 
-  const images = blok.images;
-  const photosContainer = document.getElementById("photos");
-  function preloadImage(image) {
-
-    images.forEach((image, index) => {
-      const img = new Image();
-      img.src = image.filename + "/m/";
-      img.alt = image.filename + "/m/";
-  
-      img.onload = () => {
-        // Append the image to the container when it's loaded
-        photosContainer.appendChild(img);
-  
-        // Preload the next image
-        const nextIndex = index + 1;
-        if (nextIndex < images.length) {
-          preloadImage(images[nextIndex]);
-        }
-      };
-    });
-  }
-
-
-  // Preload the first image
-  preloadImage(images[0]);
-
   return (
     <div
       style={{ display: "flex", justifyContent: "space-around" }}
@@ -37,20 +11,15 @@ const Images = ({ blok }) => {
       className="grid"
     >
       <div id="photos">
-        {/* {blok.images.map((image) => {
+        {blok.images.map((image) => {
           return (
-            <img
+            <LazyLoadImage
+             
               alt={image.filename + "/m/"}
-              src={image.filename + "/m/"}
-              loading="lazy"
+              src={image.filename + "/m/"} // use normal <img> attributes as props
             />
-            // <LazyLoadImage
-            //   effect="opacity"
-            //   alt={image.filename + "/m/"}
-            //   src={image.filename + "/m/"} // use normal <img> attributes as props
-            // />
           );
-        })} */}
+        })}
       </div>
     </div>
   );
