bbf6c5180e4cb7bc0a47dfd6ea7420ae3c5b17d9
Patrik Horný <patulda111@gmail.com>
2023-10-15 17:50:33 +0200

    Update imageCard.js

--- src/components/imageCard.js
+++ src/components/imageCard.js
@@ -10,8 +10,8 @@ export default function imageCard({ blok }) {
     >
 
            <img
-              alt={image?.filename + "/m/"}
-              src={image?.filename + "/m/"}
+              alt={image.filename + "/m/"}
+              src={image.filename + "/m/"}
               loading="lazy"
             />
     {/* <LazyLoadImage
